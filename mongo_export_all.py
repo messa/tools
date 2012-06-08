696743405d5a8e42f539003645ea45e6b92d04f4
Petr Messner <petr.messner@gmail.com>
2012-06-08 10:39:52 +0200

    Debug dump tool for MongoDB

---
+++ mongo_export_all.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import optparse
+from pprint import pformat
+import pymongo
+
+
+INDENT = " " * 4
+
+
+def main():
+    op = optparse.OptionParser()
+    op.add_option("--host", default="localhost")
+    op.add_option("--port", default="27017")
+    (options, args) = op.parse_args()
+
+    conn = pymongo.Connection(host=options.host, port=int(options.port))
+
+    print
+    for dbName in conn.database_names():
+        db = conn[dbName]
+        export_database(db)
+
+
+def export_database(db):
+    print "Database:", db.name
+    print
+    for collectionName in db.collection_names():
+        collection = db[collectionName]
+        export_collection(collection)
+
+
+def export_collection(collection):
+    print INDENT + "Collection:", collection.name
+    for doc in collection.find():
+        lines = pformat(doc).splitlines()
+        for line in lines:
+            print INDENT + INDENT + line
+    print
+
+
+
+if __name__ == "__main__":
+    main()
+
+
