c14981f97e0253b44c0cc2a784f332536ce91895
Petr Messner <petr.messner@gmail.com>
2015-06-22 09:26:04 +0200

    instant_mongodb: support for wiredtiger engine

--- instant_mongodb.py
+++ instant_mongodb.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+import argparse
 import logging
 import optparse
 import os
@@ -7,46 +8,52 @@ from os.path import isdir
 import subprocess
 
 
-localhost = "127.0.0.1"
-logger = logging.getLogger("instant_mongodb")
+localhost = '127.0.0.1'
+logger = logging.getLogger('instant_mongodb' if __name__ == '__main__' else __name__)
 
 
 def main():
-    op = optparse.OptionParser()
-    op.add_option("--datadir", "-d", default="mongo-data")
-    op.add_option("--port", "-p", default="7017")
-    op.add_option("--verbose", "-v", default=False, action="store_true")
-    op.add_option("--auth", "-a", default=False, action="store_true", help="run with security")
-    options, args = op.parse_args()
+    p = argparse.ArgumentParser()
+    p.add_argument('--datadir', '-d', default='mongo-data')
+    p.add_argument('--port', '-p', default='7017')
+    p.add_argument('--verbose', '-v', action='store_true')
+    p.add_argument('--auth', '-a', action='store_true', help='run with security')
+    p.add_argument('--wiredTiger', '-w', action='store_true')
+    args = p.parse_args()
 
-    setup_logging(verbose=options.verbose)
+    setup_logging(verbose=args.verbose)
 
-    p = mongodb_process(data_dir=options.datadir, port=int(options.port), auth=options.auth)
+    p = mongodb_process(
+        data_dir=args.datadir, port=int(args.port),
+        auth=args.auth, wiredtiger=args.wiredTiger)
     p.wait()
 
 
 def setup_logging(verbose):
-    r = logging.getLogger("")
+    r = logging.getLogger('')
     r.addHandler(logging.StreamHandler())
     r.setLevel(logging.DEBUG if verbose else logging.INFO)
 
 
-def mongodb_process(data_dir, port, auth):
+def mongodb_process(data_dir, port, auth, wiredtiger):
     if not isdir(data_dir):
         os.mkdir(data_dir)
     cmd = [
-        "mongod",
-        "--port", str(port),
-        "--bind_ip", localhost,
-        "--nounixsocket",
-        "--dbpath", data_dir,
-        "--smallfiles"]
+        'mongod',
+        '--port', str(port),
+        '--bind_ip', localhost,
+        '--nounixsocket',
+        '--dbpath', data_dir,
+        '--smallfiles']
     if auth:
-        cmd.append("--auth")
+        cmd.append('--auth')
+    if wiredtiger:
+        cmd.extend(['--storageEngine', 'wiredTiger'])
+        cmd.extend(['--wiredTigerCacheSizeGB', '1'])
     p = subprocess.Popen(cmd)
     return p
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
 
