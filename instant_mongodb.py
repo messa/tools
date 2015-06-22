1a154019c590d81aa11b5c84d366ce7a515c81e5
Petr Messner <petr.messner@gmail.com>
2015-06-22 09:28:36 +0200

    instant_mongodb cleanup

--- instant_mongodb.py
+++ instant_mongodb.py
@@ -1,38 +1,32 @@
 #!/usr/bin/env python3
 
+'''
+This is just a thin wrapper around `mongod`.
+
+Parameters are set with respect to typical development settings (smallfiles, localhost).
+'''
+
 import argparse
-import logging
-import optparse
 import os
 from os.path import isdir
 import subprocess
 
 
 localhost = '127.0.0.1'
-logger = logging.getLogger('instant_mongodb' if __name__ == '__main__' else __name__)
 
 
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('--datadir', '-d', default='mongo-data')
     p.add_argument('--port', '-p', default='7017')
-    p.add_argument('--verbose', '-v', action='store_true')
     p.add_argument('--auth', '-a', action='store_true', help='run with security')
     p.add_argument('--wiredTiger', '-w', action='store_true')
     args = p.parse_args()
-
-    setup_logging(verbose=args.verbose)
 
     p = mongodb_process(
         data_dir=args.datadir, port=int(args.port),
         auth=args.auth, wiredtiger=args.wiredTiger)
     p.wait()
-
-
-def setup_logging(verbose):
-    r = logging.getLogger('')
-    r.addHandler(logging.StreamHandler())
-    r.setLevel(logging.DEBUG if verbose else logging.INFO)
 
 
 def mongodb_process(data_dir, port, auth, wiredtiger):
