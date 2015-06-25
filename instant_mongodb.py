55256660c6629973a8b41ea4d5e56b4b41fe3f4b
Petr Messner <petr.messner@gmail.com>
2015-06-25 13:45:11 +0200

    instant_mongodb: added option --zlib for WiredTiger with zlib

--- instant_mongodb.py
+++ instant_mongodb.py
@@ -21,11 +21,12 @@ def main():
     p.add_argument('--port', '-p', default='7017')
     p.add_argument('--auth', '-a', action='store_true', help='run with security')
     p.add_argument('--wiredTiger', '-w', action='store_true')
+    p.add_argument('--zlib', '-z', action='store_true', help='wiredTiger only: use zlib compression')
     args = p.parse_args()
 
     p = mongodb_process(
         data_dir=args.datadir, port=int(args.port),
-        auth=args.auth, wiredtiger=args.wiredTiger)
+        auth=args.auth, wiredtiger=args.wiredTiger, use_zlib=args.zlib)
     try:
         p.wait()
     except KeyboardInterrupt:
@@ -33,7 +34,7 @@ def main():
         p.wait()
 
 
-def mongodb_process(data_dir, port, auth, wiredtiger):
+def mongodb_process(data_dir, port, auth, wiredtiger, use_zlib):
     if not isdir(data_dir):
         os.mkdir(data_dir)
     cmd = [
@@ -48,6 +49,8 @@ def mongodb_process(data_dir, port, auth, wiredtiger):
     if wiredtiger:
         cmd.extend(['--storageEngine', 'wiredTiger'])
         cmd.extend(['--wiredTigerCacheSizeGB', '1'])
+        if use_zlib:
+            cmd.extend(['--wiredTigerCollectionBlockCompressor', 'zlib'])
     p = subprocess.Popen(cmd)
     return p
 
