d39fb27b32ec070b9072f35fa7cf78b4bf9131a2
Petr Messner <petr.messner@gmail.com>
2017-12-12 14:45:14 +0100

    upgrade macos_ramdisk.py to Python 3

--- macos_ramdisk.py
+++ macos_ramdisk.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python2.7
-# -*- coding: UTF-8 -*-
+#!/usr/bin/env python3
 
-"""
+'''
 You can add this fragment to your ~/.bash_profile to setup TMPDIR env.
 variable automatically:
 
@@ -13,36 +12,38 @@ variable automatically:
         fi
     fi
     unset ramdisk
-"""
+'''
 
-import optparse
+import argparse
 import os
 import re
 import subprocess
 import sys
 
-assert hasattr(subprocess, "check_output"), "Are you using Python 2.7+?"
+
+class AppError (Exception):
+    pass
 
 
 def parse_size(s):
-    """
+    '''
     Velikost zadanou v jakýchkoliv obvyklých jednotkách vrací v bajtech.
-    """
-    assert isinstance(s, basestring)
+    '''
+    assert isinstance(s, str)
     m = re.match(r'^([0-9]+) *([^0-9]*)$', s)
     if not m:
-        raise Exception("Failed to parse size %r" % s)
+        raise AppError('Failed to parse size {!r}'.format(s))
     n = int(m.group(1))
     unit = m.group(2)
     if not unit:
         return n
-    if unit.lower() in ("k", "kb"):
+    if unit.lower() in ('k', 'kb'):
         return n * 2**10
-    if unit.lower() in ("m", "mb"):
+    if unit.lower() in ('m', 'mb'):
         return n * 2**20
-    if unit.lower() in ("g", "gb"):
+    if unit.lower() in ('g', 'gb'):
         return n * 2**30
-    raise Exception("Unknown unit %s" % unit)
+    raise AppError('Unknown unit {}'.format(unit))
 
 
 assert parse_size("1k") == 2**10
@@ -58,47 +59,46 @@ assert parse_size("1GB") == 2**30
 
 
 def main():
-    if sys.platform != "darwin":
-        print "Warning: this script is designed for darwin (Mac OS X); your " \
-            "platform is %s" % sys.platform
+    if sys.platform != 'darwin':
+        sys.exit(
+            'This script is designed for darwin (macOS); '
+            'you are using {}'.format(sys.platform))
 
-    op = optparse.OptionParser()
-    op.add_option("--mountpoint", default="/Volumes/ramdisk")
-    op.add_option("--size", default="400M")
-    (options, args) = op.parse_args()
+    p = argparse.ArgumentParser()
+    p.add_argument('--mountpoint', default='/Volumes/ramdisk')
+    p.add_argument('--size', default='600M')
+    args = p.parse_args()
 
-    size = parse_size(options.size)
-    if already_mounted(options.mountpoint):
-        raise Exception("Some filesystem is already mounted at %s" % options.mountpoint)
-    create_directory(options.mountpoint)
+    size = parse_size(args.size)
+    if already_mounted(args.mountpoint):
+        raise AppError('Some filesystem is already mounted at {}'.format(args.mountpoint))
+    create_directory(args.mountpoint)
     device_name = create_ramdisk_device(size)
     create_filesystem(device_name)
-    mount(device_name, options.mountpoint)
-    print "Done."
-    print ""
-    print "If you want to use the ramdisk as TMPDIR, run these commands:"
-    print ""
-    print "TMPDIR=%s; export TMPDIR" % options.mountpoint
-    tmpdir = os.getenv("TMPDIR")
+    mount(device_name, args.mountpoint)
+    print('Done.')
+    print()
+    print('If you want to use the ramdisk as TMPDIR, run these commands:')
+    print()
+    print('TMPDIR={}; export TMPDIR'.format(args.mountpoint))
+    tmpdir = os.getenv('TMPDIR')
     if tmpdir:
-        print ""
-        print "(Current TMPDIR is %s)" % tmpdir
+        print()
+        print('(Current TMPDIR is {})'.format(tmpdir))
 
 
 def create_directory(path):
     if not os.path.isdir(path):
-        print "Creting directory %s" % path
+        print('Creating directory {}'.format(path))
         os.mkdir(path)
 
 
 def already_mounted(mountpoint):
-    output = subprocess.check_output(["mount"])
-    for n, line in enumerate(output.splitlines()):
-        m = re.match(r"^(.*) on (/.*) (\(.*\))$", line)
+    output = check_output(['mount'])
+    for n, line in enumerate(output.decode().splitlines()):
+        m = re.match(r'^(.*) on (/.*) (\(.*\))$', line)
         if not m:
-            raise Exception(
-                "Failed to parse line %d of output of the command mount: %s" %
-                (n, line))
+            raise AppError('Failed to parse line {} of output of the command mount: {}'.format(n, line))
         location = m.group(2)
         if location == mountpoint:
             return True
@@ -107,36 +107,39 @@ def already_mounted(mountpoint):
 
 def create_ramdisk_device(size):
     assert isinstance(size, int)
-    print "Creating ramdisk device with size %.2f MB" % (size / 2.**20)
+    print('Creating ramdisk device with size {:.2f} MB'.format(size / 2**20))
     sectors = size / 512
-    output = subprocess.check_output(
-        ["hdiutil", "attach", "-nomount", "ram://%d" % sectors])
-    output = output.strip()
-    if not re.match(r"^/dev/disk[0-9]+$", output):
-        raise Exception("Invalid hdiutil output: %r", output)
-    print "Created device %s" % output
+    output = check_output(
+        ['hdiutil', 'attach', '-nomount', 'ram://{}'.format(sectors)])
+    output = output.decode().strip()
+    if not re.match(r'^/dev/disk[0-9]+$', output):
+        raise Exception('Invalid hdiutil output: {!r}'.format(output))
+    print('Created device {}'.format(output))
     return output
 
 
 def create_filesystem(device_name):
-    print "Checking whether %s contains no data" % device_name
-    with open(device_name, "r") as f:
+    print('Checking whether {} contains no data'.format(device_name))
+    with open(device_name, 'rb') as f:
         block = f.read(4096)
-        assert block == "\0" * len(block)
-    print "Creating HFS filesystem on %s" % device_name
-    subprocess.check_output(["newfs_hfs", device_name])
+        assert block == b'\0' * len(block)
+    print('Creating HFS filesystem on {}'.format(device_name))
+    check_output(['newfs_hfs', device_name])
 
 
 def mount(device_name, mountpoint):
-    print "Mounting %s on %s" % (device_name, mountpoint)
-    subprocess.check_output(
-        ["mount", "-o", "noatime", "-t", "hfs", device_name, mountpoint])
+    print('Mounting {} on {}'.format(device_name, mountpoint))
+    check_output(
+        ['mount', '-o', 'noatime', '-t', 'hfs', device_name, mountpoint])
 
 
-if __name__ == "__main__":
+def check_output(cmd):
+    print('>', ' '.join(cmd))
+    return subprocess.check_output(cmd)
+
+
+if __name__ == '__main__':
     try:
         main()
-    except Exception, e:
-        print >>sys.stderr, "Error: %s" % e
-        exit(1)
-
+    except AppError as e:
+        sys.exit('Error: {}'.format(e))
